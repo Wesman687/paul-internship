8588f130dd45101092200f3aeee4aefe574ef6d7
Paul <wesman687@gmail.com>
2024-07-23 00:01:29 -0400

    Setup axios fetch to access the item, changed all the info to accept information dynamically, setup skeleton state.

--- src/components/author/AuthorItems.jsx
+++ src/components/author/AuthorItems.jsx
@@ -1,19 +1,27 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import AuthorImage from "../../images/author_thumbnail.jpg";
-import nftImage from "../../images/nftImage.jpg";
+import React  from "react";
+import { Link} from "react-router-dom";
+import Skeleton from "../UI/Skeleton";
 
-const AuthorItems = () => {
+const AuthorItems = ( { author, loading }) => {
   return (
     <div className="de_tab_content">
       <div className="tab-1">
-        <div className="row">
-          {new Array(8).fill(0).map((_, index) => (
+        <div className="row">{loading ? 
+              new Array(8).fill(0).map((array, index) => (
+                <div
+                    key={index}
+                    className="d-item col-lg-3 col-md-6 col-sm-6 col-xs-12"
+                    style={{ display: "block", backgroundSize: "cover" }}
+                  >
+                      <Skeleton width="100%" height="400px" />
+                  </div>
+                )) :
+          author.nftCollection && author.nftCollection.map((array, index) => (
             <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={index}>
               <div className="nft__item">
                 <div className="author_list_pp">
                   <Link to="">
-                    <img className="lazy" src={AuthorImage} alt="" />
+                    <img className="lazy" src={author.authorImage} alt="" />
                     <i className="fa fa-check"></i>
                   </Link>
                 </div>
@@ -35,22 +43,22 @@ const AuthorItems = () => {
                       </div>
                     </div>
                   </div>
-                  <Link to="/item-details">
+                  <Link to={`/item-details/${array.nftId}`}>
                     <img
-                      src={nftImage}
+                      src={array.nftImage}
                       className="lazy nft__item_preview"
                       alt=""
                     />
                   </Link>
                 </div>
                 <div className="nft__item_info">
-                  <Link to="/item-details">
-                    <h4>Pinky Ocean</h4>
+                  <Link to={`/item-details/${array.nftId}`}>
+                    <h4>{array.title}</h4>
                   </Link>
-                  <div className="nft__item_price">2.52 ETH</div>
+                  <div className="nft__item_price">{array.price} ETH</div>
                   <div className="nft__item_like">
                     <i className="fa fa-heart"></i>
-                    <span>97</span>
+                    <span>{array.likes}</span>
                   </div>
                 </div>
               </div>
--- src/pages/ItemDetails.jsx
+++ src/pages/ItemDetails.jsx
@@ -1,13 +1,31 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import EthImage from "../images/ethereum.svg";
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import AuthorImage from "../images/author_thumbnail.jpg";
 import nftImage from "../images/nftImage.jpg";
+import axios from "axios";
+import Skeleton from "../components/UI/Skeleton";
 
 const ItemDetails = () => {
+  const { id } = useParams()
+  const [loading, setLoading] = useState(false);
+  const [nftApi, setNftApi] = useState([]);
+
+  async function fetchData() {
+    setLoading(true);
+    const data = await axios.get(
+      `https://us-central1-nft-cloud-functions.cloudfunctions.net/itemDetails?nftId=${id}`
+    );
+    const result = data.data;
+    setNftApi(result);
+    setLoading(false);
+  }
+
   useEffect(() => {
     window.scrollTo(0, 0);
+    fetchData();
   }, []);
+  console.log(nftApi)
 
   return (
     <div id="wrapper">
@@ -16,44 +34,97 @@ const ItemDetails = () => {
         <section aria-label="section" className="mt90 sm-mt-0">
           <div className="container">
             <div className="row">
+              {!loading ? 
+              <>
+              <div className="col-md-6 text-center">
+              <Skeleton width="100%" height="100%"/>
+            </div>
+            <div className="col-md-6">
+              <div className="item_info">
+                <Skeleton width="300px" height="40px" />
+
+                <div className="item_info_counts">
+                  
+                    <Skeleton width="80px" height="30px" />
+                  
+                    <Skeleton width="80px" height="30px" />
+                </div>
+                <p>
+                <Skeleton width="100%" height="80px" />
+                </p>
+                <div className="d-flex flex-row">
+                  <div className="mr40">
+                    <h6>Owner</h6>
+                    <div className="item_author">
+                      <div className="author_list_pp">
+                      <Skeleton width="50px" height="50px" borderRadius="50%" />
+                      </div>
+                      <div className="author_list_info">
+                      <Skeleton width="125px" height="20px" />
+                      </div>
+                    </div>
+                  </div>
+                  <div></div>
+                </div>
+                <div className="de_tab tab_simple">
+                  <div className="de_tab_content">
+                    <h6>Creator</h6>
+                    <div className="item_author">
+                      <div className="author_list_pp">
+                      <Skeleton width="50px" height="50px" borderRadius="50%" />
+                      </div>
+                      <div className="author_list_info">
+                      <Skeleton width="125px" height="20px" />
+                      </div>
+                    </div>
+                  </div>
+                  <div className="spacer-40"></div>
+                  <h6>Price</h6>
+                  <div className="nft-item-price">
+                  <Skeleton width="75px" height="20px" />
+                  </div>
+                </div>
+              </div>
+            </div>
+            </>
+              : 
+              <>
               <div className="col-md-6 text-center">
                 <img
-                  src={nftImage}
+                  src={nftApi.nftImage}
                   className="img-fluid img-rounded mb-sm-30 nft-image"
                   alt=""
                 />
               </div>
               <div className="col-md-6">
                 <div className="item_info">
-                  <h2>Rainbow Style #194</h2>
+                  <h2>{nftApi.title}</h2>
 
                   <div className="item_info_counts">
                     <div className="item_info_views">
                       <i className="fa fa-eye"></i>
-                      100
+                      {nftApi.views}
                     </div>
                     <div className="item_info_like">
                       <i className="fa fa-heart"></i>
-                      74
+                      {nftApi.likes}
                     </div>
                   </div>
                   <p>
-                    doloremque laudantium, totam rem aperiam, eaque ipsa quae ab
-                    illo inventore veritatis et quasi architecto beatae vitae
-                    dicta sunt explicabo.
+                    {nftApi.description}
                   </p>
                   <div className="d-flex flex-row">
                     <div className="mr40">
                       <h6>Owner</h6>
                       <div className="item_author">
                         <div className="author_list_pp">
-                          <Link to="/author">
-                            <img className="lazy" src={AuthorImage} alt="" />
+                          <Link to={`/author/${nftApi.ownerId}`}>
+                            <img className="lazy" src={nftApi.ownerImage} alt="" />
                             <i className="fa fa-check"></i>
                           </Link>
                         </div>
                         <div className="author_list_info">
-                          <Link to="/author">Monica Lucas</Link>
+                          <Link to={`/author/${nftApi.ownerId}`}>{nftApi.ownerName}</Link>
                         </div>
                       </div>
                     </div>
@@ -64,13 +135,13 @@ const ItemDetails = () => {
                       <h6>Creator</h6>
                       <div className="item_author">
                         <div className="author_list_pp">
-                          <Link to="/author">
-                            <img className="lazy" src={AuthorImage} alt="" />
+                          <Link to={`/author/${nftApi.creatorId}`}>
+                            <img className="lazy" src={nftApi.creatorImage} alt="" />
                             <i className="fa fa-check"></i>
                           </Link>
                         </div>
                         <div className="author_list_info">
-                          <Link to="/author">Monica Lucas</Link>
+                          <Link to={`/author/${nftApi.creatorId}`}>{nftApi.creatorName}</Link>
                         </div>
                       </div>
                     </div>
@@ -78,11 +149,12 @@ const ItemDetails = () => {
                     <h6>Price</h6>
                     <div className="nft-item-price">
                       <img src={EthImage} alt="" />
-                      <span>1.85</span>
+                      <span>{nftApi.price}</span>
                     </div>
                   </div>
                 </div>
               </div>
+              </>}
             </div>
           </div>
         </section>
