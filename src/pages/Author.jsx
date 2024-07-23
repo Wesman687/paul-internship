4a8e20bbc396baa248db2455f630fcdb50c104bb
Paul <wesman687@gmail.com>
2024-07-22 22:57:01 -0400

    Added fetch axios api, Set Dynamic information, Only 8 items display, Setup load more to display 4 more items everytime it is clicked, and to dissapear when it has no more items.  Setup filter search from the backend for prices and most liked.

--- src/pages/Author.jsx
+++ src/pages/Author.jsx
@@ -1,10 +1,40 @@
-import React from "react";
-import AuthorBanner from "../images/author_banner.jpg";
+import React, { useEffect, useState } from "react";
 import AuthorItems from "../components/author/AuthorItems";
-import { Link } from "react-router-dom";
-import AuthorImage from "../images/author_thumbnail.jpg";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import Skeleton from "../components/UI/Skeleton";
 
 const Author = () => {
+  const { id } = useParams();
+  const [loading, setLoading] = useState(false);
+  const [author, setAuthor] = useState([]);
+  const [followers, setFollowers] = useState("");
+  const [follower, setFollower] = useState(false)
+  useEffect(() => {
+    fetchData();
+  }, []);
+  async function fetchData() {
+    setLoading(true);
+    const data = await axios.get(
+      `https://us-central1-nft-cloud-functions.cloudfunctions.net/authors?author=${id}`
+    );
+    const result = data.data;
+    
+    setAuthor(result);
+    setLoading(false);    
+    setFollowers(result.followers)
+  }
+  function decrementFollower(){    
+    setFollower(false)
+    setFollowers(followers - 1)
+
+  }
+  function increaseFollower(){   
+    setFollower(true) 
+    setFollowers(followers + 1)
+  }
+
+
   return (
     <div id="wrapper">
       <div className="no-bottom no-top" id="content">
@@ -15,25 +45,54 @@ const Author = () => {
           aria-label="section"
           className="text-light"
           data-bgimage="url(images/author_banner.jpg) top"
-          style={{ background: `url(${AuthorBanner}) top` }}
+          style={{ background: `url(AuthorBanner) top` }}
         ></section>
-
+        
         <section aria-label="section">
           <div className="container">
             <div className="row">
-              <div className="col-md-12">
+            {loading ? <div className="col-md-12">
                 <div className="d_profile de-flex">
                   <div className="de-flex-col">
-                    <div className="profile_avatar">
-                      <img src={AuthorImage} alt="" />
+                  <div className="profile_avatar">
+                      <Skeleton width="150px" height="150px" borderRadius="50%" />
 
                       <i className="fa fa-check"></i>
                       <div className="profile_name">
                         <h4>
-                          Monica Lucas
-                          <span className="profile_username">@monicaaaa</span>
+                          <Skeleton width="200px" />
+                          <span className="profile_username">
+                          <Skeleton width="100px" /></span>
                           <span id="wallet" className="profile_wallet">
-                            UDHUHWudhwd78wdt7edb32uidbwyuidhg7wUHIFUHWewiqdj87dy7
+                          
+                          <Skeleton width="250px" />
+                          </span>
+                         
+                        </h4>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="profile_follow de-flex">
+                    <div className="de-flex-col">
+                      <div className="profile_follower">{followers} followers</div>
+                      <Skeleton width="150px" height="40px" />
+                    </div>
+                  </div>
+                </div>
+              </div> : 
+              <div className="col-md-12">
+                <div className="d_profile de-flex">
+                  <div className="de-flex-col">
+                  <div className="profile_avatar">
+                      <img src={author.authorImage} alt="" />
+
+                      <i className="fa fa-check"></i>
+                      <div className="profile_name">
+                        <h4>
+                          {author.authorName}
+                          <span className="profile_username">@{author.tag}</span>
+                          <span id="wallet" className="profile_wallet">
+                            {author.address}
                           </span>
                           <button id="btn_copy" title="Copy Text">
                             Copy
@@ -44,18 +103,20 @@ const Author = () => {
                   </div>
                   <div className="profile_follow de-flex">
                     <div className="de-flex-col">
-                      <div className="profile_follower">573 followers</div>
-                      <Link to="#" className="btn-main">
+                      <div className="profile_follower">{followers} followers</div>
+                      {follower? <button onClick={()=>decrementFollower()} className="btn-main">
+                        Un Follow
+                      </button> :<button onClick={()=>increaseFollower()} className="btn-main">
                         Follow
-                      </Link>
+                      </button>}
                     </div>
                   </div>
                 </div>
-              </div>
+              </div>}
 
               <div className="col-md-12">
                 <div className="de_tab tab_simple">
-                  <AuthorItems />
+                  <AuthorItems author={author} loading={loading}/>
                 </div>
               </div>
             </div>
